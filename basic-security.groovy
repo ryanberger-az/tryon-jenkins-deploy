3b6ab1f2790c0ef75f25843942c1e9e4005f3eb0
Ryan Berger <ryan.berger@tryonsolutions.com>
2020-03-20 16:39:03 -0400

    Adding Groovy script

---
+++ basic-security.groovy
@@ -0,0 +1,24 @@
+basic-security.groovy
+
+#!groovy
+import jenkins.model.*
+import hudson.security.*
+import jenkins.install.InstallState
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+// Create user with custom pass
+def user = instance.getSecurityRealm().createAccount('admin', 'hell0W0rlD2020')
+user.save()
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+if (!instance.installState.isSetupComplete()) {
+  println '--> Neutering SetupWizard'
+  InstallState.INITIAL_SETUP_COMPLETED.initializeState()
+}
+
+instance.save()
